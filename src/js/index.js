6a203d054ce06dfa91d9dff21b0e464d409e2d3c
yakupkalayci <ykpklyc18@gmail.com>
2023-11-02 13:18:28 +0300

    add img

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,4 @@ import * as bootstrap from 'bootstrap'
 
 import LOGO from '../images/logo.png';
 import HOMEIMG from '../images/illustration-intro.svg';
+import BGPATTERN from '../images/bg-tablet-pattern.svg'
